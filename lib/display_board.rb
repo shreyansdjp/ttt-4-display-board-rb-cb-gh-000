9147e92e410f0ef081ab871331ca2f9221cb940a
Shreyans Jain <gamingeyefinity@gmail.com>
2017-02-02 14:16:07 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,25 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board( position )
+    print " #{position[0]} "
+    print "|"
+    print " #{position[1]} "
+    print "|"
+    puts " #{position[2]} "
+    puts "-----------"
+    print " #{position[3]} "
+    print "|"
+    print " #{position[4]} "
+    print "|"
+    puts " #{position[5]} "
+    puts "-----------"
+    print " #{position[6]} "
+    print "|"
+    print " #{position[7]} "
+    print "|"
+    puts " #{position[8]} "
+end
+
+
+board = [" "," "," "," "," "," "," "," "," "]
+display_board(board)
